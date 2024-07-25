77f31da6288fca5591f95e26a283cf8621f6b694
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-25 22:58:36 +0400

    button colour

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,9 @@ export default function Navbar() {
     return (
         <div className="nav-bar">
             <Link href={'/'} className="logo"><h1><b>MetaCognify</b></h1></Link>
-            <button className="button">Log In</button>
+            <p>About</p>
+            <p>Falcon AI</p>
+            <button className="log-in-button">Log In</button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="text-section">
         <h1><b>MetaCognify</b></h1>
         <p>Students of all ages, seize your moment. Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models. Discover the power within you to excel beyond limits.</p>
-        <button className="button">Sign Up Now</button>
+        <button className="sign-up-button">Sign Up Now</button>
       </div>
       <div className="image-section">
         <Image src="/frontpage.png" alt="Front Page" width={600} height={400} />
