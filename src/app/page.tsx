82824af405ecb16986ef51a9415dd178f67f5909
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-25 22:48:41 +0400

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="main-content">
       <div className="text-section">
         <h1><b>MetaCognify</b></h1>
-        <p>Students of all ages, this is your calling. Are you struggling to retain information? Is it hard to understand concepts?  Walk yourself through powerful self-study techniques backed by psychology and optimise your progress using powerful AI models.</p>
+        <p>Students of all ages, seize your moment. Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models. Discover the power within you to excel beyond limits.</p>
+        <button className="button">Sign Up Now</button>
       </div>
       <div className="image-section">
         <Image src="/frontpage.png" alt="Front Page" width={600} height={400} />
       </div>
-
     </main>
   );
 }
