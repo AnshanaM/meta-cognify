5fb621e661c65ec382b384a682c95f35c9a9a1b7
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-25 22:30:09 +0400

    Purple bg

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <Link href={'/'} className="logo"><h1>MetaCognify</h1></Link>
+            <Link href={'/'} className="logo"><h1><b>MetaCognify</b></h1></Link>
             <button className="button">Log In</button>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="main-content">
+      <div className="text-section">
+        <h1><b>MetaCognify</b></h1>
+        <p>Students of all ages, this is your calling. Are you struggling to retain information? Is it hard to understand concepts?  Walk yourself through powerful self-study techniques backed by psychology and optimise your progress using powerful AI models.</p>
+      </div>
+      <div className="image-section">
+        <Image src="/frontpage.png" alt="Front Page" width={600} height={400} />
+      </div>
+
     </main>
   );
 }
