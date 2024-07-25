7a8c79486088b15a67c070cfe054460b733ff002
AnshanaM <anshana.manoharan.12262.11i@gmail.com>
2024-07-25 20:59:19 +0400

    navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <Link href={'/'} className="logo">MetaCognify</Link>
+            <Link href={'/'} className="logo"><h1>MetaCognify</h1></Link>
             <button className="button">Log In</button>
         </div>
     )
