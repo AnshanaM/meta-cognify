5c4f511ad6e1f1411431c1dd5d53127cb8a299bb
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 10:08:28 +0400

    Front end upgrade

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <Link href={'/'} className="logo"><h1><b>MetaCognify</b></h1></Link>
+            <Link href={'/'} className="logo">
+                <h1 className="underline-custom"><b>MetaCognify</b></h1>
+            </Link>
             <button className="log-in-button"><b>Log In</b></button>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,10 @@ export default function Home() {
   return (
     <main className="main-content">
       <div className="text-section">
+        <h2>Students of all ages, this is your calling.</h2>
         <h1><b>Revise. Retain. Revolutionize.</b></h1>
-        <p>Students of all ages, this is your calling. Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
-        <p>Discover the power within you to excel beyond limits.</p>
+        <p>Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
+        <p><i>Discover the power within you to excel beyond limits.</i></p>
         <div className="button-group">
           <button className="learn-more-button"><b>Learn More</b></button>
           <button className="sign-up-button"><b>Sign Up</b></button>
