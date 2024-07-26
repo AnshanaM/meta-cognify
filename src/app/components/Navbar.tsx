4a1c151ad2430c7443f1f78440551a252d582175
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 15:25:23 +0400

    Logo+other frontend updates

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,12 +3,10 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <div className="logo">
                 <Link href={'/'}>
                     {/* <img src="/logo.png" alt="Logo"/> */}
                     <h1 className="logo-text">MetaCognify</h1>
                 </Link>
-            </div>
             <div className="nav-bar-content">
                 <p className="nav-bar-item">About Falcon AI</p>
                 <p className="nav-bar-item">Github</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,25 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="main-content">
-      <div className="text-section">
-      {/* <p className="text-section-p">Students of all ages, this is your calling.</p> */}
-        <div className="slogan-group">
-          <h1 className="text-gradient-underline"><b>Revise.</b></h1>
-          <h1 className="text-gradient-underline"><b>Retain.</b></h1>
-          <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
+    <main>
+      <div className="main-content">
+        <div className="text-section">
+        {/* <p className="text-section-p">Students of all ages, this is your calling.</p> */}
+          <div className="slogan-group">
+            <h1 className="text-gradient-underline"><b>Revise.</b></h1>
+            <h1 className="text-gradient-underline"><b>Retain.</b></h1>
+            <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
+          </div>
+          <p>Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
+          <p><i>Discover the power within you to excel beyond limits.</i></p>
+          <div className="button-group">
+            <button className="sign-up-button">Sign Up</button>
+            <button className="learn-more-button">Learn More</button>
+          </div>
         </div>
-        <p>Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
-        <p><i>Discover the power within you to excel beyond limits.</i></p>
-        <div className="button-group">
-          <button className="sign-up-button">Sign Up</button>
-          <button className="learn-more-button">Learn More</button>
+        <div className="image-section">
+          <Image src="/frontpage.png" alt="Front Page" width={800} height={533} />
         </div>
-      </div>
-      <div className="image-section">
-        <Image src="/frontpage.png" alt="Front Page" width={800} height={533} />
       </div>
     </main>
   );
