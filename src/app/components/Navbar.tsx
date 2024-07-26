1f28aac3f1a18b321e0a3e43ab6cb7ad3c9c7aa1
AnshanaM <anshana.manoharan.12262.11i@gmail.com>
2024-07-26 11:43:48 +0400

    ui enchancement

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,8 @@ export default function Navbar() {
     return (
         <div className="nav-bar">
             <Link href={'/'} className="logo">
-                <h1 className="underline-custom"><b>MetaCognify</b></h1>
+                {/* <h1 className="text-gradient-underline"><b>MetaCognify</b></h1> */}
+                <img src="/logo.png"/>
             </Link>
             <button className="log-in-button"><b>Log In</b></button>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ export default function Home() {
   return (
     <main className="main-content">
       <div className="text-section">
-      <p>Students of all ages, this is your calling.</p>
+      {/* <p className="text-section-p">Students of all ages, this is your calling.</p> */}
         <div className="slogan-group">
-          <h1 className="underline-custom"><b>Revise. Retain. Revolutionize.</b></h1>
+          <h1 className="text-gradient-underline"><b>Revise.</b></h1>
+          <h1 className="text-gradient-underline"><b>Retain.</b></h1>
+          <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
         </div>
         <p>Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
         <p><i>Discover the power within you to excel beyond limits.</i></p>
         <div className="button-group">
-          <button className="learn-more-button"><b>Learn More</b></button>
-          <button className="sign-up-button"><b>Sign Up</b></button>
+          <button className="learn-more-button">Learn More</button>
+          <button className="sign-up-button">Sign Up</button>
         </div>
       </div>
       <div className="image-section">
