5b24ddff111c2943ef34b934763c05cb5add1a44
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 21:35:30 +0400

    Front end upgrade - content refined and rearrangement of landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <h1 className="text-gradient-underline"><b>Retain.</b></h1>
             <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
           </div>
-          <p>Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
+          <p>Students of all ages, this is your calling. Struggling to grasp or retain new knowledge? MetaCognify guides you through proven self-study techniques powered by AI to boost your understanding and track your progress.</p>
           <p><i>Discover the power within you to excel beyond limits.</i></p>
           <div className="button-group">
             <button className="sign-up-button">Sign Up</button>
@@ -24,11 +24,20 @@ export default function Home() {
 
       {/* New heading and paragraph */}
       <section className="about-techniques">
-        <h1 className="heading-text"><i>"Too often, we teach students what to think, not how to think."</i></h1>
+        <i><h1 className="heading-text">"Too often, we teach students what to think, not how to think."</h1></i>
         <p className="description-text">
-          MetaCognify allows you to actively identify what is missing from your understanding of any topic. The mind is a strong asset, but we don't have a manual.
+        Metacognition is a higher level of thinking.
+        It is the understanding and awareness of one’s thought process. Advanced learning is powered by metacognition.
+        Using MetaCognify, understand any topic of your preference at an advanced level.
+        Start now - choose any of the following powerful self-study techniques backed by psychology research and see how far your understanding goes with the help of AI.
         </p>
       </section>
+
+      {/* New section with dark background */}
+      <section className="additional-section">
+        {/* Add any content you want for this section */}
+      </section>
+
       <div className="footer">
         <p>Created by The Falconers</p>
         <p>Powered by Falcon AI</p>
