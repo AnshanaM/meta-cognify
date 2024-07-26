89e71b9df9b7ad8fc9bfb551c4a5fdaf584f0c43
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 09:47:14 +0400

    updates to landing page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="nav-bar">
             <Link href={'/'} className="logo"><h1><b>MetaCognify</b></h1></Link>
-            <button className="log-in-button">Log In</button>
+            <button className="log-in-button"><b>Log In</b></button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,13 @@ export default function Home() {
         <h1><b>Revise. Retain. Revolutionize.</b></h1>
         <p>Students of all ages, this is your calling. Are you facing challenges in retaining information or grasping complex concepts? Elevate your learning journey with groundbreaking self-study techniques rooted in psychology, and supercharge your progress with cutting-edge AI models.</p>
         <p>Discover the power within you to excel beyond limits.</p>
-        <button className="sign-up-button">Learn More</button>
+        <div className="button-group">
+          <button className="learn-more-button"><b>Learn More</b></button>
+          <button className="sign-up-button"><b>Sign Up</b></button>
+        </div>
       </div>
       <div className="image-section">
-        <Image src="/frontpage.png" alt="Front Page" width={600} height={400} />
+        <Image src="/frontpage.png" alt="Front Page" width={800} height={533} />
       </div>
     </main>
   );
