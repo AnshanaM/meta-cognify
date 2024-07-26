9c2f6471029065c820e0fcfd1c133451d2c45578
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 13:02:05 +0400

    Additional upgrades to landing page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,11 +3,17 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <Link href={'/'} className="logo">
-                {/* <h1 className="text-gradient-underline"><b>MetaCognify</b></h1> */}
-                <img src="/logo.png"/>
-            </Link>
-            <button className="log-in-button"><b>Log In</b></button>
+            <div className="logo">
+                <Link href={'/'}>
+                    {/* <img src="/logo.png" alt="Logo"/> */}
+                    <h1 className="logo-text">MetaCognify</h1>
+                </Link>
+            </div>
+            <div className="nav-bar-content">
+                <p className="nav-bar-item">About Falcon AI</p>
+                <p className="nav-bar-item">Github</p>
+            </div>
+            <button className="log-in-button">Log In</button>
         </div>
     )
 }
