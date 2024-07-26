780fcd3972355dacc4dbb352cb5e63f3a5496419
Anshali <anshali.manoharan.12263.11i@gmail.com>
2024-07-26 23:27:03 +0400

    blurred pseudo element

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="nav-bar">
-                <Link href={'/'}>
-                    {/* <img src="/logo.png" alt="Logo"/> */}
-                    <h1 className="logo-text">MetaCognify</h1>
-                </Link>
-            <div className="nav-bar-content">
-                <p className="nav-bar-item">About</p>
-                <p className="nav-bar-item">Models</p>
-                <p className="nav-bar-item">Github</p>
-            </div>
-            <button className="log-in-button">Log In</button>
+        <Link href={'/'}>
+            {/* <img src="/logo.png" alt="Logo"/> */}
+            <h1 className="logo-text">MetaCognify</h1>
+        </Link>
+        <div className="nav-bar-content">
+            <p className="nav-bar-item">About</p>
+            <p className="nav-bar-item">Models</p>
+            <p className="nav-bar-item">Github</p>
         </div>
+        <button className="log-in-button">Log In</button>
+    </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <h1 className="text-gradient-underline"><b>Retain.</b></h1>
             <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
           </div>
-          <p>Students of all ages, this is your calling. Struggling to grasp or retain new knowledge? MetaCognify guides you through proven self-study techniques powered by AI to boost your understanding and track your progress.</p>
+          <p>Students of all ages, this is your calling. Struggling to grasp or retain new knowledge? Boost your understanding and track your progress using our <b className="falcon-text">Falcon</b>-powered self-study tools</p>
           <p><i>Discover the power within you to excel beyond limits.</i></p>
           <div className="button-group">
             <button className="sign-up-button">Sign Up</button>
@@ -24,13 +24,17 @@ export default function Home() {
 
       {/* New heading and paragraph */}
       <section className="about-techniques">
-        <i><h1 className="heading-text">"Too often, we teach students what to think, not how to think."</h1></i>
-        <p className="description-text">
-        Metacognition is a higher level of thinking.
-        It is the understanding and awareness of one’s thought process. Advanced learning is powered by metacognition.
-        Using MetaCognify, understand any topic of your preference at an advanced level.
-        Start now - choose any of the following powerful self-study techniques backed by psychology research and see how far your understanding goes with the help of AI.
-        </p>
+        <Image src="/left.png" alt="Left Image" width={150} height={150} className="left-image" />
+        <div className="text-container">
+          <i><b><h1 className="heading-text">
+            "Too often, we teach students <span className="highlight-text">what</span> to think, not <span className="highlight-text">how</span> to think."
+          </h1></b></i>
+          <p className="description-text">
+            Metacognition means understanding how you learn. MetaCognify uses Artificial Intelligence to help you master any topic with proven self-study techniques. Start now and boost your learning!
+          </p>
+        </div>
+
+        <Image src="/right.png" alt="Right Image" width={150} height={150} className="right-image" />
       </section>
 
       {/* New section with dark background */}
