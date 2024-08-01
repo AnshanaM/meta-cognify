e08048e4d00f0084dbffaeed5f2e9391aa0f289f
AnshanaM <anshana.manoharan.12262.11i@gmail.com>
2024-08-02 00:00:43 +0400

    navbar and models pages

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,34 @@
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
 import "../styles/NavBar.css";
+
 export default function Navbar() {
-    return (
-        <div className="nav-bar">
-        <Link href={'/'}>
-            {/* <img src="/logo.png" alt="Logo"/> */}
-            <h1 className="logo-text">MetaCognify</h1>
-        </Link>
-        <div className="nav-bar-content">
-            <p className="nav-bar-item">About</p>
-            <p className="nav-bar-item">Models</p>
-            <p className="nav-bar-item">Github</p>
-        </div>
-        <button className="log-in-button">Log In</button>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="nav-bar">
+      <Link href={'/'}>
+        <h1 className="logo-text">MetaCognify</h1>
+      </Link>
+      <div className={`nav-bar-content ${menuOpen ? 'active' : ''}`}>
+        <p className="nav-bar-item">About</p>
+        <a href="https://falconllm.tii.ae/falcon-180b.html" className="nav-bar-item" target="_blank" rel="noopener noreferrer">
+          Models
+        </a>
+        <p className="nav-bar-item">Github</p>
+      </div>
+      <button className="log-in-button">Log In</button>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-    )
+  );
 }
