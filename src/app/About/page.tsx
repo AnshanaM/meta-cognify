7fe7669e1cf3f44bdd1bc26c2473e865d2712d13
AnshanaM <anshana.manoharan.12262.11i@gmail.com>
2024-08-02 14:45:32 +0400

    About page and landing page CSS updated

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/navigation';
 import Footer from "../components/Footer";
 
 export default function About() {
-
   const router = useRouter();
 
   return (
@@ -17,13 +16,13 @@ export default function About() {
             <div className="slogan-group">
               <b><h1 className="text-gradient-underline">The Feynman Technique</h1></b>
             </div>
-            <br/>
+            <br />
             <i><p className="paragraph-style">"The ultimate test of your knowledge is your capacity to convey it to others."</p></i>
-            <br/>
+            <br />
             <p className="paragraph-style">
-            Test your understanding by teaching a topic to Noobert, our AI bot. As you explain, Noobert asks questions a beginner would ask. At the end of your explanation, find out how well you did!
+              Test your understanding by teaching a topic to Noobert, our AI bot. As you explain, Noobert asks questions a beginner would ask. At the end of your explanation, find out how well you did!
             </p>
-            <br/>
+            <br />
             <button className="learn-more-button">Try Now!</button>
           </div>
           <div className="blocks">
@@ -38,17 +37,17 @@ export default function About() {
               </div>
               <div className="block block-gradient">
                 <div className="icon">
-                  <Image src="/explain.gif" alt="Front Page" width={70} height={70} unoptimized/>
+                  <Image src="/explain.gif" alt="Front Page" width={70} height={70} unoptimized />
                 </div>
                 <div className="block-text">
-                  <b>Noobert asks beginner questions, and analyses your explanation.</b>
+                  <b>Noobert asks beginner questions and analyzes your explanation.</b>
                 </div>
               </div>
             </div>
             <div className="cols">
               <div className="block block-gradient">
                 <div className="icon">
-                  <Image src="/analyse.gif" alt="Front Page" width={80} height={80} unoptimized/>
+                  <Image src="/analyse.gif" alt="Front Page" width={80} height={80} unoptimized />
                 </div>
                 <div className="block-text">
                   <b>Explain the concept to Noobert.</b>
@@ -56,39 +55,73 @@ export default function About() {
               </div>
               <div className="block block-black">
                 <div className="icon">
-                  <Image src="/check.gif" alt="Front Page" width={50} height={50} unoptimized/>
+                  <Image src="/check.gif" alt="Front Page" width={50} height={50} unoptimized />
                 </div>
                 <div className="block-text">
                   <b>Get a detailed analysis and feedback on possible knowledge gaps!</b>
                 </div>
               </div>
-          </div>
+            </div>
           </div>
         </div>
       </section>
 
       <section>
         <div className="socrates">
-          <div className="image">
-            <Image src="/NoobertCollage.jpeg" alt="Front Page" width={500} height={500} unoptimized/>
+        <div className="blocks">
+            <div className="cols">
+              <div className="block block-black">
+                <div className="icon">
+                  <Image src="/ask.gif" alt="Ask Questions" width={50} height={50} unoptimized />
+                </div>
+                <div className="block-text">
+                  <b>Ask thought-provoking questions.</b>
+                </div>
+              </div>
+              <div className="block block-gradient">
+                <div className="icon">
+                  <Image src="/explore.gif" alt="Explore Topics" width={70} height={70} unoptimized />
+                </div>
+                <div className="block-text">
+                  <b>Explore topics through inquiry.</b>
+                </div>
+              </div>
+            </div>
+            <div className="cols">
+              <div className="block block-gradient">
+                <div className="icon">
+                  <Image src="/understand.gif" alt="Understand Concepts" width={80} height={80} unoptimized />
+                </div>
+                <div className="block-text">
+                  <b>Gain deeper understanding.</b>
+                </div>
+              </div>
+              <div className="block block-black">
+                <div className="icon">
+                  <Image src="/discover.gif" alt="Discover Insights" width={50} height={50} unoptimized />
+                </div>
+                <div className="block-text">
+                  <b>Discover new insights and perspectives.</b>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="description">
             <b><h1 className="text-gradient-underline">Socratic Method</h1></b>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <i><p className="paragraph-style">“A technique of philosophical investigation that emphasizes the use of conversation and inquiry to explore difficult ideas and concepts.”</p></i>
-            <br/>
+            <br />
             <p className="paragraph-style">
-            Is there a challenging topic you want to explore for the first time? Have a thoughtful conversation with SocraBot, who will craft questions in a way that you will eventually discover and realise the concept on your own!
+              Is there a challenging topic you want to explore for the first time? Have a thoughtful conversation with SocraBot, who will craft questions in a way that you will eventually discover and realize the concept on your own!
             </p>
-            <br/>
+            <br />
             <button className="log-in-button">Try Now!</button>
           </div>
         </div>
       </section>
 
       <Footer />
-
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
       <div className="main-content">
         <div className="text-section">
           <div className="slogan-group">
-            <h1 className="text-gradient-underline"><b>Revise.</b></h1>
+            <h1 className="text-gradient-underline"><b>Realise.</b></h1>
             <h1 className="text-gradient-underline"><b>Retain.</b></h1>
             <h1 className="text-gradient-underline"><b>Revolutionize.</b></h1>
           </div>
-          <p className="paragraph-style">Struggling to grasp new knowledge? Boost your understanding and track your progress using our <b className="falcon-text">Falcon</b>-powered self-study tools.</p>
+          <p className="paragraph-style">Metacognition is high level thinking. It means understanding your own thinking process — key to mastering any subject. Use MetaCognify to boost your understanding and track your progress with our Falcon AI-powered self-study tools.</p>
           <p><i>Discover the power within you to excel beyond limits.</i></p>
           <div className="button-group">
             {/* <button className="sign-up-button">Sign Up</button> */}
@@ -29,37 +29,29 @@ export default function Home() {
         </div>
       </div>
 
-      <section className="about-techniques">
-        <Image src="/feynman.png" alt="Left Image" width={300} height={300} className="left-image" />
-        <div className="text-container">
-          <i><b><h1 className="heading-text">
-            "Too often, we teach students <span className="highlight-text">what</span> to think, not <span className="highlight-text">how</span> to think."
-          </h1></b></i>
-          <p className="description-text">
-            Metacognition is a higher level of thinking. It is the <span className="highlight-text">understanding and awareness of one’s thought process.</span> This is crucial if you wish to have an advanced level of understanding of concepts, regardless of which field. From the <span className="highlight-text">Feynman Technique</span> to the <span className="highlight-text">Socratic Method</span>, use MetaCognify to understand any topic of your choice, and track your progress with Falcon AI. 
-          </p>
-        </div>
-        <Image src="/socrates.png" alt="Right Image" width={300} height={300} className="right-image" />
-      </section>
-
       <section className="services">
         <b><p>THE ULTIMATE SELF-STUDY RESOURCE</p></b>
         <i><b><h1 className="heading-text">
           "Too often, we teach students <span className="highlight-text">what</span> to think, not <span className="highlight-text">how</span> to think."
         </h1></b></i>
-        <h3>Finding the best learning approach for you</h3>
+        <h3>With MetaCognify, achieve deeper understanding and critical thinking skills.</h3>
         <div className="cards-container">
           <div className="card">
-            PUT IMAGE HERE
-            <p>put text here</p>
+          <Image src="/theBots.png" alt="Intro to bots image" width={300} height={190} style={{ borderRadius: '15px' }}></Image>
+          <h3>Chat Bots Designed for Self-Study</h3>
+            <p>Meet Noobert and Socrabot, our charming AI chatbots designed to simulate the Feynman Technique and the Socratic Method.</p>
           </div>
           <div className="card">
-            PUT IMAGE HERE
-            <p>put text here</p>
+          <Image src="/feature22.png" alt="Analytics image" width={300} height={190} style={{ borderRadius: '15px' }}
+/>            <b><h3>Personalized Learning Analytics</h3></b>
+            <p>Using feedback from AI, see how well you understand and explain concepts based on conversations with the chatbots.
+            </p>
           </div>
           <div className="card">
-            PUT IMAGE HERE
-            <p>put text here</p>
+            <Image src="/interaction.png" alt="Interaction image" width={300} height={190} style={{ borderRadius: '15px' }}></Image>
+            <b><h3>Interactive Learning Experience</h3></b>
+            <p>Experience an engaging and interactive learning process designed to boost your understanding and perspective of any topic.
+            </p>
           </div>
         </div>
       </section>
